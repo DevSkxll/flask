fadc923a94306f1ed071c5a0fe250d7bfe2becb3
DevSkxll <suki202346@gmail.com>
2025-11-08 15:27:50 -0600

    Styling

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
+
+app.secret_key = b'\xf2\x9a\xbb\x19\x12\xbd\xaa\xe2K\xae/V\xbaV\x11\x0f\xa2~\x99\xacU\x1a\xd1\x99\x0c\x1e\x1e[\xb3N\xd4\x8c'
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,13 +22,28 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/about')
 def about():
     return render_template('/about.html', company_name='gearmindsacademy.com')
 
-@app.route('/add_stock')
+@app.route('/add_stock' , methods=['GET' , 'POST'])
 def add_stock():
+    if request.method == 'POST':
+        # Print the form data to the console
+        for key, value in request.form.items():
+            print(f'{key}: {value}')
+
+        session['stock_symbol'] = request.form['stock_symbol']
+        session['number_of_shares'] = request.form['number_of_shares']
+        session['purchase_price'] = request.form['purchase_price']
+        return redirect(url_for('list_stocks'))  #NEW!!
+        
     return render_template('add_stock.html')
+
+@app.route('/stocks/')
+def list_stocks():
+    return render_template('stocks.html')
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
